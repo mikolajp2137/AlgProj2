10bfd4e5e8b84923f938b59092df526db35c756e
mikolajp2137 <73973110+mikolajp2137@users.noreply.github.com>
2023-01-15 22:05:27 +0100

    done

--- src/BinaryTreeA.java
+++ src/BinaryTreeA.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BinaryTreeA {
     static class Node {
         int value;
@@ -36,5 +39,45 @@ public class BinaryTreeA {
         System.out.print(node.value + "\n ");
         print(node.left);
         print(node.right);
+    }
+
+    public void insertBalanced(ArrayList<Integer> arrayList){
+        Collections.sort(arrayList);
+        //arrayList.forEach(System.out::println);
+        insertSorted(arrayList,0,arrayList.size()-1);
+    }
+
+    public void insertSorted(ArrayList<Integer> arrayList, int start, int end) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            insert(arrayList.get(mid));
+            insertSorted(arrayList, start, mid - 1);
+            insertSorted(arrayList, mid + 1, end);
+        }
+    }
+
+    public void clearTree(Node root) {
+        if (root == null) {
+            return;
+        }
+        clearTree(root.left);
+        clearTree(root.right);
+        root.left = null;
+        root.right = null;
+        root = null;
+    }
+
+    public boolean contains(Node root, int val) {
+        if (root == null) {
+            return false;
+        }
+        if (root.value == val) {
+            return true;
+        }
+        if (val < root.value) {
+            return contains(root.left, val);
+        } else {
+            return contains(root.right, val);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,94 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         BinaryTreeA binTreeA = new BinaryTreeA();
+        BinaryTreeA binTreeB = new BinaryTreeA();
+        TreeSet binTreeC = new TreeSet();
         Random random = new Random();
-        int n = 10;
+        int m = 13;
+        int n = (int) (Math.pow(2, m)) - 1;
+        System.out.println("Number of elements: " + ((int) (Math.pow(2, m)) - 1));
         ArrayList<Integer> val = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            val.add(i, random.nextInt(100) + 1);
+
+        int counter = 100;
+        long totalOfTimesFA, totalOfTimesFB, totalOfTimesFC, totalOfTimesA, totalOfTimesB, totalOfTimesC;
+        totalOfTimesA = totalOfTimesB = totalOfTimesC = totalOfTimesFA = totalOfTimesFB = totalOfTimesFC = 0;
+        long startTime, endTime, duration;
+
+        while (counter > 0) {
+            val.clear();
+            binTreeA.clearTree(binTreeA.root);
+            binTreeB.clearTree(binTreeB.root);
+            binTreeC.clear();
+            //inserting
+            for (int i = 0; i < n; i++) {
+                val.add(i, random.nextInt(100) + 1);
+            }
+
+            startTime = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                binTreeA.insert(val.get(i));
+            }
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesA = totalOfTimesA + duration;
+
+            startTime = System.nanoTime();
+            binTreeB.insertBalanced(val);
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesB += duration;
+
+            startTime = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                binTreeC.add(val.get(i));
+            }
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesC += duration;
+
+
+            //finding
+            startTime = System.nanoTime();
+            for(int i =0;i<n;i++){
+                if(!binTreeB.contains(binTreeA.root,val.get(i))){
+                    System.out.println(val.get(i) + " not found");
+                }
+            }
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesFA += duration;
+
+            startTime = System.nanoTime();
+            for(int i =0;i<n;i++){
+                if(!binTreeB.contains(binTreeB.root,val.get(i))){
+                    System.out.println(val.get(i) + " not found");
+                }
+            }
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesFB += duration;
+
+            startTime = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                if (!binTreeC.contains(val.get(i))) {
+                    System.out.println(val.get(i) + " not found");
+                }
+            }
+            endTime = System.nanoTime();
+            duration = endTime - startTime;
+            totalOfTimesFC += duration;
+
+            counter--;
         }
 
-        long startTime = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            binTreeA.insert(val.get(i));
-        }
-        long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println("Execution time in nanoseconds: " + duration);
-
-        //binTreeA.print(binTreeA.root);
+        System.out.println("Average key insertion time for a) : " + totalOfTimesA / 100);
+        System.out.println("Average key insertion time for b) : " + totalOfTimesB / 100);
+        System.out.println("Average key insertion time for c) : " + totalOfTimesC / 100);
+        System.out.println("\n");
+        System.out.println("Average key finding time for a) : " + totalOfTimesFA / 100);
+        System.out.println("Average key finding time for b) : " + totalOfTimesFB / 100);
+        System.out.println("Average key finding time for c) : " + totalOfTimesFC / 100);
     }
 }
